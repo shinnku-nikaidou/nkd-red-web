c701d59d1ec3c49f11cca2101d0309b48d180a43
shinnku <nikaidou@shinnku.com>
2023-11-11 08:16:18 +0800

    update counter

--- src/components/home/Counter.tsx
+++ src/components/home/Counter.tsx
@@ -1,22 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Counter.css";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export default function Counter({
-  children,
-  count: initialCount,
-}: {
-  children: JSX.Element;
+interface CounterState {
   count: number;
-}) {
-  const [count, setCount] = useState(initialCount);
-  const add = () => setCount((i) => i + 1);
-  const subtract = () => setCount((i) => i - 1);
+  setCount: (newCount: number) => void;
+}
+
+const useCounterState = create<CounterState>()(
+  persist(
+    (set) => ({
+      count: 0,
+      setCount: (newCount: number) => set({ count: newCount }),
+    }),
+    {
+      name: "home-count", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+    }
+  )
+);
+
+export default function Counter({ children }: { children: JSX.Element }) {
+  const [initCount, setInitCount] = useState<number>(0);
+  const { count, setCount } = useCounterState();
+  useEffect(() => {
+    setInitCount(count);
+  });
+
+  const add = () => setCount(count + 1);
+  const subtract = () => {
+    alert("我就不让你一直点减");
+    setCount(count - 1);
+  };
 
   return (
     <>
       <div className="counter">
         <button onClick={subtract}>-</button>
-        <pre>{count}</pre>
+        <pre>{initCount}</pre>
         <button onClick={add}>+</button>
       </div>
       <div className="counter-message">{children}</div>
